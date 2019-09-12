8ace844bd9806f19d61a05a74a682d507f7b49b4
Jonathan Lim <jonlim@notonthehighstreet.com>
2019-09-12 11:38:47 +0100

    Allow a timeout to be passed in

--- lib/contentstack/api.rb
+++ lib/contentstack/api.rb
@@ -6,11 +6,12 @@ require 'open-uri'
 
 module Contentstack
   class API
-    def self.init_api(api_key, access_token, environment,host)
+    def self.init_api(api_key, access_token, environment, host, options={})
       @host = host
       @api_version = '/v3'
       @environment = environment
       @headers = {api_key: api_key, access_token: access_token, user_agent: "ruby-sdk/#{Contentstack::VERSION}", environment: @environment}
+      @open_timeout = options[:open_timeout] || 5
     end
 
     def self.fetch_content_types(uid="")
@@ -47,7 +48,14 @@ module Contentstack
       query = "?" + q.contentstack_to_query
       # puts "Request URL:- #{@host}#{@api_version}#{path}#{query} \n\n"
 
-      ActiveSupport::JSON.decode(open("#{@host}#{@api_version}#{path}#{query}").read)
+      response = open(
+        "#{@host}#{@api_version}#{path}#{query}",
+        {
+          :open_timeout => @open_timeout
+        }
+      )
+
+      ActiveSupport::JSON.decode(response.read)
     end
   end
 end
